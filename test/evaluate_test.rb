f33ba7df459091d711968b37bb376b4188953ea9
HoneyryderChuck <cardoso_tiago@hotmail.com>
2023-01-05 23:14:51 +0000

    specific error message for violation of reserved words

--- test/evaluate_test.rb
+++ test/evaluate_test.rb
@@ -111,6 +111,9 @@ class CelEvaluateTest < Minitest::Test
     assert_equal environment(a: :timestamp)
       .evaluate("a", { a: Google::Protobuf::Timestamp.new(seconds: 1_671_926_400) }),
                  Time.parse("2022-12-25T00:00:00Z")
+
+    err = assert_raises(Cel::ParseError) { environment(size: :int).evaluate("break == 2", { size: 2 }) }
+    assert_match /invalid usage of the reserved word "break"/, err.message
   end
 
   def test_condition
